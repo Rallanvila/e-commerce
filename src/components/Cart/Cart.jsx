1284f6a70a59fa742824bca8720e231a9887c868
Allan Vila <allanvila@Allans-MBP.tampabay.rr.com>
2022-03-14 17:23:55 -0400

    implemented cart route

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CartItem from "./CartItem/CartItem";
 import { Container, Typography, Button, Grid } from "@material-ui/core";
 
 import useStyles from "./styles";
@@ -20,7 +21,7 @@ export default function Cart({ cart }) {
 				<Grid container spacing={3}>
 					{cart?.line_items.map((item) => (
 						<Grid item xs={12} sm={4} key={item.id}>
-							<div>{item.name}</div>
+							<CartItem item={item} />
 						</Grid>
 					))}
 				</Grid>
@@ -56,7 +57,7 @@ export default function Cart({ cart }) {
 	return (
 		<Container>
 			<div className={classes.toolbar} />
-			<Typography className={classes.title} variant="h3">
+			<Typography className={classes.title} variant="h3" gutterBottom>
 				Your Shopping Cart
 			</Typography>
 			{!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
