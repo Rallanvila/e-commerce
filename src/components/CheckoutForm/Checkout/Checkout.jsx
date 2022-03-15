2a35be9a24ba13c2cb8186be36d0dd979dc02e09
Allan Vila <allanvila@Allans-MBP.tampabay.rr.com>
2022-03-15 10:47:00 -0400

    stripe is integrated for sandbox payments

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -15,7 +15,7 @@ import PaymentForm from "../PaymentForm";
 import { commerce } from "../../../lib/commerce";
 
 const steps = ["Shipping address", "Payment details"];
-const Checkout = ({ cart }) => {
+const Checkout = ({ cart, order, onCaptureCheckout, error }) => {
 	const [activeStep, setActiveStep] = useState(0);
 	const [checkoutToken, setCheckoutToken] = useState(null);
 	const [shippingData, setShippingData] = useState({});
@@ -51,7 +51,13 @@ const Checkout = ({ cart }) => {
 		activeStep === 0 ? (
 			<AddressForm checkoutToken={checkoutToken} next={next} />
 		) : (
-			<PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} />
+			<PaymentForm
+				shippingData={shippingData}
+				checkoutToken={checkoutToken}
+				backStep={backStep}
+				nextStep={nextStep}
+				onCaptureCheckout={onCaptureCheckout}
+			/>
 		);
 
 	return (
